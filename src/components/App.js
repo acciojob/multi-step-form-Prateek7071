0c4de7c06e85a2489a243456fb6ac785ab0f01b4
Prateek7071 <tiwariprateek123@gmail.com>
2026-07-01 01:06:20 +0530

    mm

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,55 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Step from "./Step.js";
 
 const App = () => {
+  const [currentStep, setCurrentStep] = useState(1)
+  
+  const handleChange = (e) => {
+    const { id, value } = e.target
+    setFormData(prev => ({
+      ...prev,
+      [id]: value
+    }))
+  }
+  
+  const handleNext = () => {
+    setCurrentStep(prev=>prev+1)
+  }
+  
+  const handlePrev = () => {
+    setCurrentStep(prev=>prev-1)
+  }
+  
+  const handleSubmit = () => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+  
+  
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    model: "",
+    car_price: "",
+    card_info: "",
+    expiry_date:""
+  })
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit}>
+        <Step
+          currentStep={currentStep}
+          formData={formData}
+          onChange={handleChange}
+          onNext={handleNext}
+          onPrevious={handlePrev}
+        >
+        </Step>
+      </form>
     </div>
   )
 }
--- src/components/Step.js
+++ src/components/Step.js
@@ -6,10 +6,10 @@ export default function Step(
   const renderFields = () => {
     switch (currentStep) {
       case 1:
-      return (
+        return (
+        <>
         <div>
-          <form>
-            <h1>Custormer2 Details</h1>
+            <h1>Custormer Details</h1>
             <label htmlFor='first_name'>First Name</label>
             <input
               type='text'
@@ -26,15 +26,94 @@ export default function Step(
               onChange={onChange}
               required
             ></input>
-            <div style={{ display: "flex", direction: "row" }}>
-              <button
-                id="nextBtn"
-                onClick={onNext}
-              >Next</button>
             </div>
-          </form>
-        </div>
-      )
+          </>
+        )
+
+      case 2:
+        return (
+        <>
+        <div>
+            <h1>Car Details</h1>
+            <label htmlFor='model'>Model</label>
+            <input
+              type='text'
+              id="model"
+              value={formData.model}
+              onChange={onChange}
+              required
+            ></input>
+            <label htmlFor='car_price'>Car Price</label>
+            <input
+              type='text'
+              id="car_price"
+              value={formData.car_price}
+              onChange={onChange}
+              required
+            ></input>
+            </div>
+          </>
+        )
+        case 3:
+          return (
+          <>
+          <div>
+              <h1>Payment Details</h1>
+              <label htmlFor='card_info'>Credit Card Number</label>
+              <input
+                type='text'
+                id="card_info"
+                value={formData.card_info}
+                onChange={onChange}
+                required
+              ></input>
+              <label htmlFor='expiry_date'>Expiration Date</label>
+              <input
+                type='text'
+                id="expiry_date"
+                value={formData.expiry_date}
+                onChange={onChange}
+                required
+              ></input>
+              </div>
+            </>
+          )
+      default:
+        return null  
     }
   }
+  return (
+    <>
+      {renderFields()}
+      <div style={{display:"flex", justifyContent:"space-between"}}>
+        {
+          currentStep > 1 && (
+            <button
+              type='button'
+              onClick={onPrevious}
+            >Previous</button>
+          )
+        }
+        {
+          currentStep < 3 && (
+            <button
+              type='button'
+              onClick={onNext}
+              style={{ marginLeft: 'auto' }}
+            >Next</button>
+          )
+        }
+        {
+          currentStep === 3 && (
+            <button
+              type='submit'
+              style={{ marginLeft: 'auto' }}
+            >
+              Submit
+            </button>
+          )
+        }
+      </div>
+    </>
+  )
 }
